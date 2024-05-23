29b776b90c4e284bb95a18a04fa9f832b97d71e9
Rob Svirskas <robsvi@gmail.com>
2024-05-23 13:12:59 -0400

    Improved version handling

---
+++ sync/update_gcr.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+REPO=dis-sync
+VERSION=0.0.9
+IMAGE=`echo $REPO:$VERSION`
+LATEST=`echo $REPO:latest`
+echo $IMAGE
+docker image build . --no-cache -t gcr.io/sandbox-220614/scsw/jenkins/$IMAGE --platform linux/amd64
+docker image ls | grep $REPO
+docker image ls | grep $REPO | grep $VERSION | awk '{print $3}' | xargs -J % -t docker image tag % gcr.io/sandbox-220614/scsw/jenkins/$IMAGE
+docker image ls | grep $REPO | grep $VERSION | awk '{print $3}' | xargs -J % -t docker image tag % gcr.io/sandbox-220614/scsw/jenkins/$LATEST
+gcloud auth login
+gcloud auth configure-docker
+CLOUDSDK_CORE_PROJECT=sandbox-220614
+gcloud config set project sandbox-220614
+docker push gcr.io/sandbox-220614/scsw/jenkins/$IMAGE
+docker push gcr.io/sandbox-220614/scsw/jenkins/$LATEST
