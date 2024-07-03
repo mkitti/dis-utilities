f0aa231be413578d94e4631b6ad8d47b660e332c
virginiascarlett <virginia.t.scarlett@gmail.com>
2024-07-03 14:48:55 -0400

    create a new name_match script that connects to the DB directly

---
+++ utility/bin/NEW_name_match.py
@@ -0,0 +1,154 @@
+"""
+New version of name_match.py that will connect to the database directly instead of making REST requests.
+"""
+
+
+import requests
+import os
+import sys
+from rapidfuzz import fuzz
+import string
+from unidecode import unidecode
+import re
+import itertools
+from termcolor import colored
+import jrc_common.jrc_common as JRC
+import doi_common.doi_common as DL
+from operator import attrgetter
+import sys
+#TODO: Add some of these to requirements.txt?
+
+################## STUFF YOU SHOULD EDIT ##################
+doi = '10.7554/eLife.80660'
+##########################################################
+
+DB = {}
+PROJECT = {}
+
+def initialize_program():
+    ''' Intialize the program
+        Keyword arguments:
+          None
+        Returns:
+          None
+    '''
+    # Database
+    try:
+        dbconfig = JRC.get_config("databases")
+    except Exception as err:
+        terminate_program(err)
+    dbs = ['dis']
+    for source in dbs:
+        manifold = 'prod'
+        dbo = attrgetter(f"{source}.{manifold}.write")(dbconfig)
+        try:
+            DB[source] = JRC.connect_database(dbo)
+        except Exception as err:
+            terminate_program(err)
+    try:
+        rows = DB['dis'].project_map.find({})
+    except Exception as err:
+        terminate_program(err)
+    for row in rows:
+        PROJECT[row['name']] = row['project']
+
+def terminate_program(msg=None):
+    ''' Terminate the program gracefully
+        Keyword arguments:
+          msg: error message
+        Returns:
+          None
+    '''
+    if msg:
+        print(msg)
+        sys.exit(-1)
+    else:
+        sys.exit(0)
+
+
+class Author:
+    def __init__(self, raw_name, orcid=None, affiliations=None, employee_id=None):
+        self.raw_name = raw_name
+        self.name = self.remove_punctuation(unidecode(raw_name))
+        self.orcid = orcid
+        self.affiliations = affiliations if affiliations is not None else [] # Need to avoid the python mutable arguments trap
+        self.employee_id = employee_id
+    
+    def remove_punctuation(self, raw_name):
+        return(raw_name.translate(str.maketrans('', '', string.punctuation)))
+
+
+class Employee:
+    def __init__(self, id, job_title=None, email=None, first_names=None, middle_names=None, last_names=None):
+        self.id = id
+        self.job_title = job_title
+        self.email = email
+        self.first_names = first_names if first_names is not None else [] # Need to avoid the python mutable arguments trap
+        self.middle_names = middle_names if middle_names is not None else []
+        self.last_names = last_names if last_names is not None else []
+    def generate_name_permutations(self):
+        #TODO: Check hyphenated lastnames, last names with spaces
+        permutations = set()
+        # All possible first names + all possible last names
+        for first_name, last_name in itertools.product(self.first_names, self.last_names):
+            permutations.add(
+                f"{first_name} {last_name}"
+            )
+        # All possible first names + all possible middle names + all possible last names
+        if self.middle_names:
+            if any(item for item in self.middle_names if item): # check for ['', '']
+                for first_name, middle_name, last_name in itertools.product(self.first_names, self.middle_names, self.last_names):
+                    permutations.add(
+                        f"{first_name} {middle_name} {last_name}"
+                    )
+            # All possible first names + all possible middle initials + all possible last names
+                for first_name, middle_name, last_name in itertools.product(self.first_names, self.middle_names, self.last_names):
+                    middle_initial = middle_name[0]
+                    permutations.add(
+                        f"{first_name} {middle_initial} {last_name}"
+                    )
+        return sorted(permutations)
+
+class Guess(Employee):
+    def __init__(self, id, job_title=None, email=None, first_names=None, middle_names=None, last_names=None, name=None, score=None):
+        super().__init__(id, job_title, email, first_names, middle_names, last_names)
+        self.name = name
+        self.score = score
+
+
+def instantiate_guess(employee, name=None, score=None):
+    return(Guess(
+        employee.id, 
+        employee.job_title, 
+        employee.email, 
+        employee.first_names, 
+        employee.middle_names, 
+        employee.last_names, 
+        name, 
+        score
+        )
+    )
+
+
+class MissingPerson:
+    """ This class indicates that searching the HHMI People API yielded no results. """
+    pass
+
+class MultipleHits:
+    """ This class indicates that an author name matched multiple HHMI employees with equally high scores. """
+    def __init__(self, winners=None):
+        self.winners = winners if winners is not None else []
+
+
+
+def search_orcid_collection(orcid):
+    DL.single_orcid_lookup(orcid, collection, 'orcid')
+
+
+
+
+# -----------------------------------------------------------------------------
+
+if __name__ == '__main__':
+    initialize_program()
+    collection = DB['dis'].orcid
