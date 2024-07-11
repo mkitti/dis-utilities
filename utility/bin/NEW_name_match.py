b3cc52816dce650b41153dd71ddea6b367feeb9b
virginiascarlett <virginia.t.scarlett@gmail.com>
2024-07-11 16:59:55 -0400

    add functions to guess employeeId

--- utility/bin/NEW_name_match.py
+++ utility/bin/NEW_name_match.py
@@ -152,28 +152,15 @@ class MultipleHits:
 
 
 
-def generic_get_request(url, headers):
-    response = requests.get(url, headers=headers)
-    if response.status_code == 200:
-        return(response.json())
-    else:
-        print(f"There was an error with the API GET request. Status code: {response.status_code}.\n Error message: {response.reason}")
-        sys.exit(1)
-
-
 def search_people_api(search_term, mode):
+    response = None
     if mode not in {'name', 'id'}:
         raise ValueError("HHMI People API search mode must be either 'name' or 'id'.")
-    url = ''
     if mode == 'name':
-        url = people_api_url + 'Search/ByName/' + search_term
+        response = JRC.call_people_by_name(search_term)
     elif mode == 'id':
-        url = people_api_url + 'Person/GetById/' + search_term
-    headers = { 'APIKey': f'{api_key}', 'Content-Type': 'application/json' }
-    response = generic_get_request(url, headers)
+        response = JRC.call_people_by_id(search_term)
     if not response:
-        #print(f"Searching the HHMI People API for {search_term} yielded no results.")
-        #sys.exit(1)
         return(MissingPerson())
     else:
         return(response)
@@ -216,6 +203,71 @@ def create_author_objects(doi_record):
     return(author_objects)
 
 
+def is_janelian(author):
+    result = False
+    if author.orcid:
+        try:
+            result = search_orcid_collection(author.orcid)
+            result = True
+        except Exception as e:
+            pass
+    if bool(re.search(r'\bJanelia\b', " ".join(author.affiliations))):
+        result = True
+    return(result)
+
+def create_employee(id):
+    idsearch_results = search_people_api(id, 'id')
+    if not isinstance(idsearch_results, MissingPerson):
+        job_title = job_title = idsearch_results['businessTitle'] if 'businessTitle' in idsearch_results else None
+        email = idsearch_results['email'] if 'email' in idsearch_results else None
+        first_names = [ idsearch_results['nameFirstPreferred'], idsearch_results['nameFirst'] ]
+        middle_names = [ idsearch_results['nameMiddlePreferred'], idsearch_results['nameMiddle'] ]
+        last_names = [ idsearch_results['nameLastPreferred'], idsearch_results['nameLast'] ]
+        return(
+            Employee(
+            id,
+            job_title=job_title,
+            email=email,
+            first_names=first_names, 
+            middle_names=middle_names, 
+            last_names=last_names)
+        )
+    else:
+        return(MissingPerson())
+
+def guess_employee(author):
+    candidate_employees = [] # Includes false positives. For example, if I search 'Virginia',
+    # both Virginia Scarlett and Virginia Ruetten will be in this list.
+    search_term  = max(author.name.split(), key=len) # We can only search the People API by one name, so just pick the longest one
+    namesearch_results = search_people_api(search_term, 'name')
+    if not isinstance(namesearch_results, MissingPerson):
+        candidate_employee_ids = [ employee_dic['employeeId'] for employee_dic in namesearch_results ]
+        for id in candidate_employee_ids:
+            employee_from_id_search = create_employee(id) 
+            if not isinstance(employee_from_id_search, MissingPerson):
+                candidate_employees.append(employee_from_id_search)
+            else:
+                return(MissingPerson)
+        guesses = []
+        for employee in candidate_employees:
+            employee_permuted_names = employee.generate_name_permutations()
+            for name in employee_permuted_names:
+                guesses.append(instantiate_guess(employee, name=name)) # Each employee will generate several guesses, e.g. Virginia T Scarlett, Virginia Scarlett
+        for guess in guesses:
+            guess.score = fuzz.token_sort_ratio(author.name.lower(), guess.name.lower())
+        high_score = max( [g.score for g in guesses] )
+        winner = [ g for g in guesses if g.score == high_score ]
+        if len(winner) > 1:
+            return(MultipleHits(winners = winner))
+        else:
+            return(winner[0])
+    else:
+        return(MissingPerson())
+
+
+
+
+
 # -----------------------------------------------------------------------------
 
 if __name__ == '__main__':
@@ -223,12 +275,25 @@ if __name__ == '__main__':
     collection = DB['dis'].orcid
     doi_record = get_doi_record(doi)
     all_authors = create_author_objects(doi_record)
+    janelian_authors = [ a for a in all_authors if is_janelian(a) ]
+    for author in janelian_authors:
+        if author.orcid:
+            mongo_orcid_record = search_orcid_collection(author.orcid, collection)
+            if mongo_orcid_record:
+                if 'employeeId' in mongo_orcid_record:
+                    pass # They will automatically be added to jrc_authors in another script
+                elif 'employeeId' not in mongo_orcid_record:
+                    #TODO! FUZZY MATCH!
+                    best_guess = guess_employee(author)
 
 #For bug testing:
 # import NEW_name_match
 # NEW_name_match.initialize_program()
+# collection = NEW_name_match.DB['dis'].orcid
 # doi_record = NEW_name_match.get_doi_record('10.7554/eLife.80660')
 # all_authors = NEW_name_match.create_author_objects(doi_record)
+# janelian_authors = [ a for a in all_authors if NEW_name_match.is_janelian(a) ]
+# 
 
 
 
