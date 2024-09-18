d280266fe9bb01fd7f1abe45dee372a6c73cdfd7
virginiascarlett <virginia.t.scarlett@gmail.com>
2024-09-18 17:27:10 -0400

    fix: wrapper script will not sync DOIs that are already in the DB.

--- utility/bin/weekly_pubs.py
+++ utility/bin/weekly_pubs.py
@@ -1,23 +1,27 @@
+''' weekly_pubs.py
+    Run all the librarian's scripts for the weekly pipeline, in order.
+    This script should live in utility/bin, and it expects update_dois.py to be in sync/bin.
+
+'''
+
 import os
+import sys
 import argparse
+import requests
+import copy
 import subprocess
 from collections.abc import Iterable
+import jrc_common.jrc_common as JRC
+from operator import attrgetter
 
-sync_bin_path = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'sync', 'bin'))
 
-PARSER = argparse.ArgumentParser(
-description = "Run the weekly pipeline for one or more DOIs: add DOI(s) to database, curate authors and tags, and print citation(s).")
-MUEXGROUP = PARSER.add_mutually_exclusive_group(required=True)
-MUEXGROUP.add_argument('--doi', dest='DOI', action='store',
-                        help='Single DOI to process')
-MUEXGROUP.add_argument('--file', dest='FILE', action='store',
-                        help='File of DOIs to process')
-PARSER.add_argument('--verbose', dest='VERBOSE', action='store_true',
-                    default=False, help='Flag, Chatty')
-PARSER.add_argument('--write', dest='WRITE', action='store_true',
-                    default=False, help='Write results to database. If --write is missing, no changes to the database will be made.')
 
-ARG = PARSER.parse_args()
+# Functions to pass command line args to subsequent python scripts
+
+def create_command(script, ARG): # will produce a list like, e.g. ['python3', 'update_dois.py', '--doi' '10.1038/s41593-024-01738-9', '--verbose']
+    return(
+        list(flatten( ['python3', script, doi_source(ARG), verbose(ARG), write(ARG)] ))
+    )
 
 
 def doi_source(ARG):
@@ -25,8 +29,6 @@ def doi_source(ARG):
         return( ['--doi', ARG.DOI] )
     elif ARG.FILE:
         return(['--file', ARG.FILE])
-    else:
-        sys.exit("ERROR: Neither --doi nor --file provided.")
 
 def verbose(ARG):
     if ARG.VERBOSE:
@@ -40,9 +42,6 @@ def write(ARG):
     else:
         return([])
 
-
-
-
 def flatten(xs): # https://stackoverflow.com/questions/2158395/flatten-an-irregular-arbitrarily-nested-list-of-lists
     for x in xs:
         if isinstance(x, Iterable) and not isinstance(x, (str, bytes)):
@@ -51,19 +50,113 @@ def flatten(xs): # https://stackoverflow.com/questions/2158395/flatten-an-irregu
             yield x
 
 
-def create_command(script, ARG): # will produce a list like, e.g. ['python3', 'update_dois.py', '--doi' '10.1038/s41593-024-01738-9', '--verbose']
-    return(
-        list(flatten( ['python3', script, doi_source(ARG), verbose(ARG), write(ARG)] ))
-    )
+
+# Functions to handle DOIs already in the database
+
+def modify_arg_for_sync(ARG):
+    arg_copy = copy.deepcopy(ARG)
+    dois = get_dois_from_commandline(ARG)
+    dois_to_sync = [ d for d in dois if not already_in_db(d) ]
+    if ARG.DOI:
+        arg_copy.DOI = dois_to_sync
+    elif ARG.FILE:
+        with open('to_sync.txt', 'w') as outF:
+            outF.write("\n".join(dois_to_sync) )
+        arg_copy.FILE = 'to_sync.txt'
+    return(arg_copy)
+
+def get_dois_from_commandline(ARG):
+    dois = [ARG.DOI.lower()] if ARG.DOI else [] # .lower() because our collection is case-sensitive
+    if ARG.FILE:
+        try:
+            with open(ARG.FILE, "r", encoding="ascii") as instream:
+                for doi in instream.read().splitlines():
+                    dois.append(doi.strip().lower())
+        except Exception as err:
+            print(f"Could not process {ARG.FILE}")
+            exit()
+    return(dois)
 
 
-# use get_doi_record to check whether its in db alreday, if it is, don't add
-#can use doi/<doi> endpoint, check 'source' key under the 'rest' key. if 'source' is 'mongo', don't add
-subprocess.call(create_command(f'{sync_bin_path}/update_dois.py', ARG))
+# Functions to query the API
 
-subprocess.call(create_command('name_match.py', ARG))
+def already_in_db(doi):
+    if get_rest_info(doi)["source"] == "mongo":
+        return(True)
+    else:
+        return(False)
 
-subprocess.call(create_command('update_tags.py', ARG))
+def get_rest_info(doi):
+    rest = JRC.get_config("rest_services")
+    url_base = attrgetter("dis.url")(rest)
+    url = f'{url_base}doi/{replace_slashes_in_doi(strip_doi_if_provided_as_url(doi))}'
+    response = get_request(url)
+    return( response['rest'] ) 
 
-subprocess.call(list(flatten( ['python3', 'get_citation.py', doi_source(ARG)] ))) 
+def replace_slashes_in_doi(doi):
+    return( doi.replace("/", "%2F") ) # e.g. 10.1186/s12859-024-05732-7 becomes 10.1186%2Fs12859-024-05732-7
+
+def strip_doi_if_provided_as_url(doi, substring=".org/10.", doi_index_in_substring = 5):
+    # Find all occurrences of the substring
+    occurrences = [i for i in range(len(doi)) if doi.startswith(substring, i)]
+    if len(occurrences) > 1:
+        print("Warning: Please check that your DOI is formatted correctly.")
+        exit(1)  # Exit with a warning code
+    elif len(occurrences) == 1:
+        doi_index_in_string = occurrences[0]
+        stripped_doi = doi[doi_index_in_string + doi_index_in_substring:]
+        return(stripped_doi)
+    else:
+        return(doi)
+
+def get_request(url):
+    headers = { 'Content-Type': 'application/json' }
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        return(response.json())
+    else:
+        print(f"There was an error with the API GET request. Status code: {response.status_code}.\n Error message: {response.reason}")
+        sys.exit(1)
+
+
+
+
+
+# -----------------------------------------------------------------------------
+
+if __name__ == '__main__':
+
+    PARSER = argparse.ArgumentParser(
+    description = "Run the weekly pipeline for one or more DOIs: add DOI(s) to database, curate authors and tags, and print citation(s).")
+    MUEXGROUP = PARSER.add_mutually_exclusive_group(required=True)
+    MUEXGROUP.add_argument('--doi', dest='DOI', action='store',
+                            help='Single DOI to process')
+    MUEXGROUP.add_argument('--file', dest='FILE', action='store',
+                            help='File of DOIs to process')
+    PARSER.add_argument('--verbose', dest='VERBOSE', action='store_true',
+                        default=False, help='Flag, Chatty')
+    PARSER.add_argument('--write', dest='WRITE', action='store_true',
+                        default=False, help='Write results to database. If --write is missing, no changes to the database will be made.')
+
+    ARG = PARSER.parse_args()
+
+# If we add a DOI to the database that is already in there, the load source will be set to 'Manual' in the metadata, 
+# which is misleading, so we don't want to add DOIs that are already in there.
+# We can use the API to check whether the DOI is already in the database.
+    sync_bin_path = os.path.normpath(os.path.join(os.path.dirname(os.path.abspath(__file__)), '..', '..', 'sync', 'bin'))
+    print(ARG.DOI, ARG.FILE)
+    arg_copy = modify_arg_for_sync(ARG)
+    print(ARG.DOI, ARG.FILE)
+    if not arg_copy.DOI and not arg_copy.FILE:
+        print('WARNING: No DOIs to add to database. Skipping sync.')
+    else:
+        subprocess.call(create_command(f'{sync_bin_path}/update_dois.py', arg_copy))
+
+    print(create_command('name_match.py', ARG))
+    print(ARG.DOI, ARG.FILE)
+    subprocess.call(create_command('name_match.py', ARG))
+
+    subprocess.call(create_command('update_tags.py', ARG))
+
+    subprocess.call(list(flatten( ['python3', 'get_citation.py', doi_source(ARG)] ))) 
 
